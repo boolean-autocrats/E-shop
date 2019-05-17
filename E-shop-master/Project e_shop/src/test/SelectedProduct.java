345243791a6c335c8069899a90b17a4bf4dda33f
Ioannis Skoumpas <ioannissk@ieee.org>
2019-05-17 23:39:47 +0300

    Add all the needed Classes

--- E-shop-master/Project e_shop/src/test/SelectedProduct.java
+++ E-shop-master/Project e_shop/src/test/SelectedProduct.java
@@ -2,7 +2,70 @@ package test;
 
 public class SelectedProduct {
 
-    public void SelectedProduct(){
+
+     public void SelectedProduct(){
+        System.out.println("asdfasd");
 
     }
+
+
+    private int code ;
+    private String name;
+    private String description;
+    private int quantity;
+    private double totalCost;
+    private double price;
+
+    // Getters
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+
+    // Setters
+    public void setTotalCost() {
+        this.totalCost = getPrice()*getQuantity();
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
 }
