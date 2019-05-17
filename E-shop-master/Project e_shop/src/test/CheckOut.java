a1f959a74d56d73ec8d1dc8ca8fe7b3729167c2c
Ioannis Skoumpas <ioannissk@ieee.org>
2019-05-17 23:41:41 +0300

    add

--- E-shop-master/Project e_shop/src/test/CheckOut.java
+++ E-shop-master/Project e_shop/src/test/CheckOut.java
@@ -5,5 +5,10 @@ public class CheckOut {
     public void invoice(){
 
 
+        System.out.println("\t\tInvoice");
+        System.out.println("\t\t----------\n");
+        System.out.println("\t\t\t-Name-\t\t\t\t-Description-\t\t\t-Price-");
+        System.out.println("\t\t\t\t");
+
     }
 }
