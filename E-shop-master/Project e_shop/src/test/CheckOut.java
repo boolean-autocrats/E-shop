02e6af792774e10af5f84588a5bd6351e7e7b97d
Ioannis Skoumpas <ioannissk@ieee.org>
2019-05-18 09:52:13 +0300

    Completed the Classes SelectedProduct and Checkout. Only Menu remains

--- E-shop-master/Project e_shop/src/test/CheckOut.java
+++ E-shop-master/Project e_shop/src/test/CheckOut.java
@@ -6,21 +6,24 @@ public class CheckOut {
 
     public void invoice(List<SelectedProduct> list){
 
+        int TotalCost = 0;
 
-        System.out.println("\t\t\t\tInvoice");
-        System.out.println("\t\t\t\t--------\n");
-        System.out.println("\t-Name-\t\t\t\t-Description-\t\t\t-Price-");
-        System.out.println("\t------\t\t\t\t-------------\t\t\t-------");
+        System.out.println("\n\t\t\t\t\t\t\tInvoice");
+        System.out.println("\t\t\t\t\t\t\t--------\n");
+        System.out.println("\t-Name-\t\t\t\t\t\t\t\t\t\t-Description-\t\t\t\t\t\t\t\t\t\t\t\t-Quantity-\t\t\t\t-Price-");
+        System.out.println("\t------\t\t\t\t\t\t\t\t\t\t-------------\t\t\t\t\t\t\t\t\t\t\t\t---------\t\t\t\t-------");
 
+        // Needs some work
         for(SelectedProduct s: list){
-            System.out.println(list.);
-            System.out.println();
-            System.out.println();
-            System.out.println();
+            System.out.printf("\t" + s.getName());
+            System.out.printf("\t\t\t\t\t\t" + s.getDescription());
+            System.out.printf("\t\t\t\t\t\t\t\t" + s.getQuantity());
+            System.out.printf("\t\t\t\t\t " + s.getPrice());
 
-
-
+            TotalCost += s.getPrice() * s.getQuantity();
         }
+
+        System.out.println("\n\n\tThe Total cost is :" + TotalCost);
 
 
 
