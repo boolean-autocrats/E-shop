65acc300c53fd85109f0822fcd100d2a8a45855d
Ioannis Skoumpas <ioannissk@ieee.org>
2019-05-22 23:26:15 +0300

    Add Alex classes. Only the phone bugs remains

--- E-shop-master/Project e_shop/src/test/MainMenu.java
+++ E-shop-master/Project e_shop/src/test/MainMenu.java
@@ -7,28 +7,27 @@ package test;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-
 public abstract class MainMenu {
     
     public static void main(String [] args){
         Scanner input = new Scanner(System.in);
         int selection;
-        
-            //printing_the_stuff..
-        System.out.println("---------Eshop Selections---------\n");
-        
+
+        //printing_the_stuff..
+        System.out.println("---------E-shop Selections---------\n");
+
         System.out.println("1)Product List..");
         System.out.println("2)Chech out");
         System.out.println("3)Sign in");
         System.out.println("0)Exit\n");
-        
-        
-            //value grabber
+
+
+        //value grabber
         System.out.print("Give me your selection :");
-        
+
         while(true){
             try {
-                selection = input.nextInt(); 
+                selection = input.nextInt();
                 if(selection >= 0 && selection <=3){
                     break;
                 }else{
@@ -39,15 +38,15 @@ public abstract class MainMenu {
                 System.out.print("Incorrect value :");
             }
         }
-        
-            //if_classes..
-            
+
+        //if_classes..
+
         if(selection == 1){
-            
+
         }else if(selection == 2){
-            
+
         }else if(selection == 3){
-            
+
         }else{  //selection == 0 (Exit)
             System.out.println("Thanks for coming!");
         }
